e1b5f6254f834a975fafd772d7f0755d4122d40f
Geisyanne <geisyannecristinap@gmail.com>
2024-03-25 16:27:58 -0300

    update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "co.geisyanne.volleymatch"
         minSdk = 26
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         signingConfig = signingConfigs.getByName("release")
