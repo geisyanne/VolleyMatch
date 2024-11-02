e71c655ef76959ea9f75e25d9a96766883c11f5f
Geisyanne <geisyannecristinap@gmail.com>
2024-11-02 12:16:02 -0300

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "co.geisyanne.volleymatch"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.1.8"
+        versionCode = 12
+        versionName = "1.1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         signingConfig = signingConfigs.getByName("release")
